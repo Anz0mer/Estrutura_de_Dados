b693d3e59c141869483a56df8de5f3b3337e1584
Anna Carolina Zomer <117862465+Anz0mer@users.noreply.github.com>
2024-03-01 14:11:48 -0300

    Update main.c

--- aula02/main.c
+++ aula02/main.c
@@ -10,3 +10,15 @@ typedef struct{
   int qtde;
   celula *primeira;
 } lista;
+
+lista *criar_lista(){
+  lista *lde = malloc(sizeof(lista));
+  lde->qtde = 0;
+  lde->primeira = NULL;
+  return lde;
+}
+
+int main(void){
+  printf("Hello World\n");
+  return 0;
+}
